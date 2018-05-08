745234029e604ca1204de03f6974a64505be78bb
nictrix <nickwillever@gmail.com>
2018-05-08 12:00:58 -0700

    make names agnostic

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "google" {
   version = "~> 1.0"                                                                                                         
 }
 
-resource "google_compute_network" "acme_corp" {
+resource "google_compute_network" "main" {
   name        = "${var.organization_name}"
   description = "${var.organization_name} network"
 
@@ -12,23 +12,23 @@ resource "google_compute_network" "acme_corp" {
 resource "google_compute_subnetwork" "public" {
   name          = "public"
   ip_cidr_range = "10.22.50.0/24"
-  network       = "${google_compute_network.acme_corp.self_link}"
+  network       = "${google_compute_network.main.self_link}"
 }
 
 resource "google_compute_subnetwork" "app" {
   name          = "app"
   ip_cidr_range = "10.22.60.0/24"
-  network       = "${google_compute_network.acme_corp.self_link}"
+  network       = "${google_compute_network.main.self_link}"
 }
 
 resource "google_compute_subnetwork" "job" {
   name          = "job"
   ip_cidr_range = "10.22.70.0/24"
-  network       = "${google_compute_network.acme_corp.self_link}"
+  network       = "${google_compute_network.main.self_link}"
 }
 
 resource "google_compute_subnetwork" "db" {
   name          = "db"
   ip_cidr_range = "10.22.80.0/24"
-  network       = "${google_compute_network.acme_corp.self_link}"
+  network       = "${google_compute_network.main.self_link}"
 }
