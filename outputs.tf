a872649c66f491aa2a40f55d62e71c1f9f3e9cc3
Aaron Lane <aaron.lane@newcontext.com>
2018-05-25 18:21:31 -0400

    Output the job subnetwork name

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "database_subnetwork_name" {
   value       = "${google_compute_subnetwork.db.name}"
 }
 
+output "job_subnetwork_name" {
+  description = "The name of the job subnetwork."
+  value       = "${google_compute_subnetwork.job.name}"
+}
+
 output "name" {
   description = "The name of the network."
   value       = "${google_compute_network.main.name}"
